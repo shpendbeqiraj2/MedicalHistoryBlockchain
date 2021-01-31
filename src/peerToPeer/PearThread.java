0804e8296911524f536c0d76a9244055003a511c
veronaav <77302084+veronaav@users.noreply.github.com>
2021-01-31 22:45:36 +0100

    Peer to Peer ++

--- src/peerToPeer/PearThread.java
+++ src/peerToPeer/PearThread.java
@@ -1,10 +1,10 @@
 package peerToPeer;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import com.google.gson.JsonObject;
-import javax.json.*;
+import javax.json.*; 
 
 public class PearThread extends Thread{
 	private BufferedReader bufferedReader;
@@ -14,8 +14,8 @@ public class PearThread extends Thread{
 	public void run() {
 		boolean flag = true;
 		while (flag) {
-			try {
-				JsonObject jsonObject = Json.createReader(bufferedReader). readObject();
+			try { 
+				javax.json.JsonObject jsonObject = Json.createReader(bufferedReader). readObject();
 				if (jsonObject.containsKey("username"))
 					System.out.println("["+jsonObject.getString("username")+"]: "+jsonObject.getString("message"));
 			} catch(Exception e) {
